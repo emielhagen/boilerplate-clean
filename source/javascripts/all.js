91a4cd5cb3abdba4966d326d358fec92e393baa3
emiel hagen <emiel.hagen@gmail.com>
2016-02-23 14:54:59 +0100

    update on profile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,11 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function () {
+  "use strict";
+
+$.backstretch([
+    "images/fotobrug.jpg"
+  ], {duration: 3800, fade: 1500});
+
+});
