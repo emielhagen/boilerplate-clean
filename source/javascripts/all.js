06fc1328d28825671fbffc8871b7b8633aa19e84
emiel hagen <emiel.hagen@gmail.com>
2016-04-11 13:56:54 +0200

    update profile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,7 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
- $(function() {
+$(document).ready(function() {
  $('a[href*="#"]:not([href="#"])').click(function() {
    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
      var target = $(this.hash);
@@ -15,4 +15,19 @@
      }
    }
  });
+ $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    if ($(window).scrollTop() > 50) {
+      $('.home-btn-hide').addClass('home-btn');
+      $('.home-btn').removeClass('home-btn-hide');
+    }
+    if ($(window).scrollTop() < 10) {
+      $('.home-btn').addClass('home-btn-hide');
+      $('.home-btn-hide').removeClass('home-btn');
+    }
+  });
 });
+
+
